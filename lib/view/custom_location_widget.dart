049352fefada94340faa9df3d601d6ff261ee968
Rex Chen <rexchen@RexdeMac-mini.local>
2024-02-15 17:36:33 +0800

    have simple location widget

--- lib/view/custom_location_widget.dart
+++ lib/view/custom_location_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_weather/model/custom_location.dart';
 import 'package:pocket_weather/model/custom_location_repository.dart';
 import 'package:pocket_weather/view_model/city_view_model.dart';
 import 'package:logger/logger.dart';
@@ -49,15 +50,23 @@ class CustomLocationWidget extends StatelessWidget {
               ),
               const Divider(),
               Expanded(
-                child: ListView.builder(
-                  itemCount: 10,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text('Item $index'),
-                      onTap: () {
-                        logger.d("one saved record is tapped");
-                      },
-                    );
+                child: FutureBuilder<Map<String, CustomLocation>>(
+                  future: customLocationViewModel.getAllCustomLocations(),
+                  builder: (context, snapshot) {
+                    final customLocations = snapshot.data ?? {};
+                    return ListView.builder(
+                        itemCount: customLocations.length,
+                        itemBuilder: (context, index) {
+                          final key = customLocations.keys.elementAt(index);
+                          final customLocation = customLocations[key];
+                          return ListTile(
+                            title: Text(customLocation?.label ?? ''),
+                            onTap: () {
+                              logger.d(
+                                  'custom location item tap ${customLocation?.label ?? ''}');
+                            },
+                          );
+                        });
                   },
                 ),
               ),
