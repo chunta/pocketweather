d0ac471068f4e0654b2a5228a7f606929fb0c67d
Rex Chen <rexchen@RexdeMac-mini.local>
2024-02-17 01:00:34 +0800

    cache lab

--- lib/model/repository/city_repository.dart
+++ lib/model/repository/city_repository.dart
@@ -9,6 +9,8 @@ import 'package:pocket_weather/model/city_current.dart';
 import 'package:pocket_weather/model/city_forecast.dart';
 import 'package:pocket_weather/model/city_location.dart';
 import 'package:pocket_weather/model/weather_condition.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio/dio.dart';
 
 class CityRepository {
   final logger = Logger();
@@ -42,7 +44,41 @@ class CityRepository {
 
   static final CityRepository _instance = CityRepository._();
 
+  static late Dio dio;
+
   factory CityRepository() {
+// Global options
+    final options = CacheOptions(
+      // A default store is required for interceptor.
+      store: MemCacheStore(),
+
+      // All subsequent fields are optional.
+
+      // Default.
+      policy: CachePolicy.forceCache,
+      // Returns a cached response on error but for statuses 401 & 403.
+      // Also allows to return a cached response on network errors (e.g. offline usage).
+      // Defaults to [null].
+      hitCacheOnErrorExcept: [401, 403, 304],
+      // Overrides any HTTP directive to delete entry past this duration.
+      // Useful only when origin server has no cache config or custom behaviour is desired.
+      // Defaults to [null].
+      maxStale: const Duration(days: 7),
+      // Default. Allows 3 cache sets and ease cleanup.
+      priority: CachePriority.normal,
+      // Default. Body and headers encryption with your own algorithm.
+      cipher: null,
+      // Default. Key builder to retrieve requests.
+      keyBuilder: CacheOptions.defaultCacheKeyBuilder,
+      // Default. Allows to cache POST requests.
+      // Overriding [keyBuilder] is strongly recommended when [true].
+      allowPostMethod: false,
+    );
+     dio = Dio()
+      ..interceptors.add(
+        DioCacheInterceptor(options: options),
+      );
+
     return _instance;
   }
 
@@ -130,6 +166,17 @@ class CityRepository {
 
   Future<CityForecast> fetchApi(String url) async {
     try {
+      //
+      dio.options.headers.remove('If-None-Match');
+      var fresponse = await dio.get("http://localhost:3000/test");
+      logger.d(fresponse.statusCode);
+      logger.d(fresponse.headers);
+      logger.d(fresponse.data);
+      dio.options.headers = {'If-None-Match': fresponse.headers['etag']};
+      fresponse = await dio.get("http://localhost:3000/test");
+      logger.d(fresponse.statusCode);
+      logger.d(fresponse.data);
+      //
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         final jsonResponse = jsonDecode(response.body);
