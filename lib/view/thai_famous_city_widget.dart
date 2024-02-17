3132757bdb3c37326bcfaadc25454eae841c577e
Rex Chen <rexchen@RexdeMac-mini.local>
2024-02-17 21:49:22 +0800

    move buttons out from ChangeNotifierProvider

--- lib/view/thai_famous_city_widget.dart
+++ lib/view/thai_famous_city_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:pocket_weather/model/repository/thai_famous_city_repository.dart';
@@ -11,20 +13,62 @@ class ThaiParentWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("(14)");
+    Logger().d("ThaiParentWidget build its body");
     return SafeArea(
         child: Column(
       children: [
         Container(
           margin: const EdgeInsets.only(top: 16.0), // Add top margin
-          child: const Text("line 18"),
+          child: const Text("Top Title"),
         ),
+        const ThaiFamousCityButtonsWidget(),
         const Expanded(
           child: ThaiFamousCityWidget(),
         ),
-        const Text("line 18"),
+        const Text("Bottom Title"),
       ],
     ));
+  }
+}
+
+class ThaiFamousCityButtonsWidget extends StatelessWidget {
+  const ThaiFamousCityButtonsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Logger().d('ThaiFamousCityButtonsWidget build its body');
+    return Column(children: [
+      const SizedBox(height: 20), // Added SizedBox for spacing
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              ThaiFamousCityViewModel()
+                  .fetchForcastByNameCase(ThaiFamousCity.bangkok);
+            },
+            child: Text(ThaiFamousCityViewModel()
+                .nameFromNameCity(ThaiFamousCity.bangkok)),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ThaiFamousCityViewModel()
+                  .fetchForcastByNameCase(ThaiFamousCity.chiangMai);
+            },
+            child: Text(ThaiFamousCityViewModel()
+                .nameFromNameCity(ThaiFamousCity.chiangMai)),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ThaiFamousCityViewModel()
+                  .fetchForcastByNameCase(ThaiFamousCity.phuket);
+            },
+            child: Text(ThaiFamousCityViewModel()
+                .nameFromNameCity(ThaiFamousCity.phuket)),
+          ),
+        ],
+      ),
+    ]);
   }
 }
 
@@ -33,67 +77,30 @@ class ThaiFamousCityWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Logger().d('ThaiFamousCityWidget build its body');
     return ChangeNotifierProvider(
       create: (_) => ThaiFamousCityViewModel(),
       child: ThaiFamousCityBodyWidget(
-        conditionManager:
-            WeatherConditionManager(conditionTable: WeatherConditionTable()),
-      ),
+          conditionManager: WeatherConditionManager(
+        conditionTable: WeatherConditionTable(),
+      )),
     );
   }
 }
 
 class ThaiFamousCityBodyWidget extends StatelessWidget {
-  final logger = Logger();
-
   final WeatherConditionManager conditionManager;
 
   ThaiFamousCityBodyWidget({super.key, required this.conditionManager});
 
-  // bool inited = false;
+  bool inited = false;
 
   @override
   Widget build(BuildContext context) {
-    print('build 35');
+    Logger().d('ThaiFamousCityBodyWidget build its body');
     return Scaffold(
       body: Column(
         children: <Widget>[
-          const SizedBox(height: 20), // Added SizedBox for spacing
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () {
-                  context
-                      .read<ThaiFamousCityViewModel>()
-                      .fetchForcastByNameCase(ThaiFamousCity.bangkok);
-                },
-                child: Text(context
-                    .watch<ThaiFamousCityViewModel>()
-                    .nameFromNameCity(ThaiFamousCity.bangkok)),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  context
-                      .read<ThaiFamousCityViewModel>()
-                      .fetchForcastByNameCase(ThaiFamousCity.chiangMai);
-                },
-                child: Text(context
-                    .watch<ThaiFamousCityViewModel>()
-                    .nameFromNameCity(ThaiFamousCity.chiangMai)),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  context
-                      .read<ThaiFamousCityViewModel>()
-                      .fetchForcastByNameCase(ThaiFamousCity.phuket);
-                },
-                child: Text(context
-                    .watch<ThaiFamousCityViewModel>()
-                    .nameFromNameCity(ThaiFamousCity.phuket)),
-              ),
-            ],
-          ),
           if (context
               .watch<ThaiFamousCityViewModel>()
               .cityForecast
@@ -115,12 +122,13 @@ class ThaiFamousCityBodyWidget extends StatelessWidget {
           Expanded(
             child: Consumer<ThaiFamousCityViewModel>(
               builder: (context, thaiCityViewModel, _) {
-                // if (!inited) {
-                //   conditionManager.initTable();
-                //   thaiCityViewModel.fetchForcastByNameCase(ThaiFamousCity.bangkok);
-                //   inited = true;
-                //   return const Center(child: CircularProgressIndicator());
-                // }
+                if (!inited) {
+                  conditionManager.initTable();
+                  thaiCityViewModel
+                      .fetchForcastByNameCase(ThaiFamousCity.bangkok);
+                  inited = true;
+                  return const Center(child: CircularProgressIndicator());
+                }
                 return ListView.builder(
                   itemCount: thaiCityViewModel
                       .cityForecast.forecast.forecastday.length,
@@ -143,90 +151,3 @@ class ThaiFamousCityBodyWidget extends StatelessWidget {
     );
   }
 }
-
-/*
-class ThaiFamousCityBodyWidget extends StatelessWidget {
-  final logger = Logger();
-
-  final WeatherConditionManager conditionManager;
-
-  ThaiFamousCityBodyWidget({super.key, required this.conditionManager});
-
-  bool inited = false;
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Consumer<ThaiFamousCityViewModel>(
-        builder: (context, thaiCityViewModel, _) {
-      print("(38)");
-      if (inited == false) {
-        conditionManager.initTable();
-        thaiCityViewModel.fetchForcastByNameCase(ThaiFamousCity.bangkok);
-        inited = true;
-         return const Center(child: CircularProgressIndicator());
-      }
-      return Scaffold(
-        body: Column(
-          children: <Widget>[
-            const SizedBox(height: 20), // Added SizedBox for spacing
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    thaiCityViewModel
-                        .fetchForcastByNameCase(ThaiFamousCity.bangkok);
-                  },
-                  child: Text(thaiCityViewModel
-                      .nameFromNameCity(ThaiFamousCity.bangkok)),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    thaiCityViewModel
-                        .fetchForcastByNameCase(ThaiFamousCity.chiangMai);
-                  },
-                  child: Text(thaiCityViewModel
-                      .nameFromNameCity(ThaiFamousCity.chiangMai)),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    thaiCityViewModel
-                        .fetchForcastByNameCase(ThaiFamousCity.phuket);
-                  },
-                  child: Text(thaiCityViewModel
-                      .nameFromNameCity(ThaiFamousCity.phuket)),
-                ),
-              ],
-            ),
-            if (thaiCityViewModel.cityForecast.location.name.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 16.0), // Adjust top margin as needed
-                child: Text(
-                  thaiCityViewModel.cityForecast.location.name,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-            Expanded(
-              child: ListView.builder(
-                itemCount:
-                    thaiCityViewModel.cityForecast.forecast.forecastday.length,
-                itemBuilder: (context, index) {
-                  final dayForecast = thaiCityViewModel
-                      .cityForecast.forecast.forecastday[index];
-                  return ListTile(
-                    title: Text(Utility.formatEpochTime(dayForecast.dateEpoch)),
-                    subtitle: Text(conditionManager.lookUpDescriptionByCode(
-                        dayForecast.dayCondition.condition.code)),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-}
-*/
